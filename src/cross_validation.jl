eed0aa109ec60f13758b16f5fda20c25b29fec04
Michael-tehc <71926248+Michael-tehc@users.noreply.github.com>
2023-09-07 15:56:32 +0300

    Now `cross_validation` uses `forecast_length` and does rolling window forecasts.

--- src/cross_validation.jl
+++ src/cross_validation.jl
@@ -1,16 +1,17 @@
 export cross_validation
 
-struct CrossValidation
-    abs_errors::Matrix{Float64}
-    mae::Vector{Float64}
-    crps_scores::Matrix{Float64}
-    mean_crps::Vector{Float64}
+struct CrossValidation{T<:AbstractFloat}
+    abs_errors::Matrix{T}
+    mae::Vector{T}
+    crps_scores::Matrix{T}
+    mean_crps::Vector{T}
+
     function CrossValidation(n::Int, steps_ahead::Int)
         abs_errors = Matrix{Float64}(undef, steps_ahead, n)
         crps_scores = Matrix{Float64}(undef, steps_ahead, n)
         mae = Vector{Float64}(undef, steps_ahead)
         mean_crps = Vector{Float64}(undef, steps_ahead)
-        return new(abs_errors, mae, crps_scores, mean_crps)
+        return new{Float64}(abs_errors, mae, crps_scores, mean_crps)
     end
 end
 
@@ -36,27 +37,37 @@ function evaluate_crps(y::Vector{T}, scenarios::Matrix{T}) where {T}
 end
 
 """
-TODO
+$TYPEDSIGNATURES
+
+Rolling window forecast.
 """
-function cross_validation(gas::ScoreDrivenModel{<:Distribution, T}, y::Vector{T}, steps_ahead::Int, start_idx::Int;
-                  S::Int = 10_000,
-                  initial_params::Matrix{T} = DEFAULT_INITIAL_PARAM,
-                  opt_method = NelderMead(gas, DEFAULT_NUM_SEEDS)) where T
+function cross_validation(
+    gas::ScoreDrivenModel{<:Distribution, T}, y::Vector{T}, forecast_length::Int, steps_ahead::Int=1;
+    S::Int = 10_000, initial_params::Matrix{T} = DEFAULT_INITIAL_PARAM,
+    opt_method = NelderMead(gas, DEFAULT_NUM_SEEDS), verbose=DEFAULT_VERBOSE
+) where T
+    @assert forecast_length > 1
+    @assert steps_ahead >= 1
+
+    check_model_estimated(gas) && throw(ArgumentError("Model must have some unestimated parameters!"))
+
+    start_idx = length(y) - forecast_length
     num_mle = length(y) - start_idx - steps_ahead
     b = CrossValidation(num_mle, steps_ahead)
-    for i in 1:num_mle
-        println("CrossValidation: step $i of $num_mle")
+    for t in 1:num_mle
+        (verbose >= 1) && @info "CrossValidation: step $t of $num_mle"
         gas_to_fit = deepcopy(gas)
-        y_to_fit = y[1:start_idx - 1 + i]
-        y_to_verify = y[start_idx + i:start_idx - 1 + i + steps_ahead]
-        fit!(gas_to_fit, y_to_fit; initial_params=initial_params, opt_method=opt_method)
-        forec = initial_params !== DEFAULT_INITIAL_PARAM ?
+        y_to_fit = y[t:start_idx - 1 + t] # window moves as `t` increases
+        y_to_verify = y[start_idx + t:start_idx - 1 + t + steps_ahead]
+        fit!(gas_to_fit, y_to_fit; initial_params, opt_method, verbose)
+
+        forec::Forecast = initial_params !== DEFAULT_INITIAL_PARAM ?
                     forecast(y_to_fit, gas_to_fit, steps_ahead; S=S, initial_params=initial_params) :
                     forecast(y_to_fit, gas_to_fit, steps_ahead; S=S)
         abs_errors = evaluate_abs_error(y_to_verify, forec.observation_forecast)
         crps_scores = evaluate_crps(y_to_verify, forec.observation_scenarios)
-        b.abs_errors[:, i] = abs_errors
-        b.crps_scores[:, i] = crps_scores
+        b.abs_errors[:, t] = abs_errors
+        b.crps_scores[:, t] = crps_scores
     end
     for i in 1:steps_ahead
         b.mae[i] = mean(b.abs_errors[i, :])
--- test/test_cross_validation.jl
+++ test/test_cross_validation.jl
@@ -4,5 +4,5 @@
     B = [0.5 0; 0 0.5]
     simulation = simulate_GAS_1_1(Normal, 0.0, ω, A, B, 1)
     gas = ScoreDrivenModel(1, 1, Normal, 0.0)
-    bac = cross_validation(gas, simulation, 10, 4985)
+    bac = cross_validation(gas, simulation, 4985, 10)
 end
